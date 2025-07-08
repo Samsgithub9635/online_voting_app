8db4b9ced01ef5c9568e9c1ce96406b99525c8db
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-08 23:07:08 +0530

    modified jwt.js to generate tokens

--- jwt.js
+++ jwt.js
@@ -1,23 +1,17 @@
 import jwt from 'jsonwebtoken';
 
 const jwtAuthMiddleware = (req, res, next)=>{
-
-    // first check request headers has authoization or not
-    const authoization = req.headers.authoization;
-    if(!authoization) return req.staus(401).json({error: 'Token Not Found'});
-
     // Extract the jwt token from the req header
     // jwt auth middleware
-    const token = req.headers.autorization.split(' ')[1];
+    const token = req.headers.autorization.split(' ')[1];// splited Bearer token use ' '(space) so that the 'bearer' keyword will be at 0th index and the actual token will be at 1st index
     if(!token) return res.staus(401).json({error: 'Unauthorized' });
 
     try{
         // Verify the JWT token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); //jwt.verify() used to verify the token taking the token and the secret key(JWT_SECRET) and returns the payload
 
         // Attach user information to the request object
-        //req.userJWTPayload = decoded //the decoded payload came we want to send it to server
-       req.user = decoded
+        req.userJWTPayload = decoded; //the decoded payload came we want to send it to server
         next();
     }catch(err){
         console.error(err);
@@ -30,7 +24,7 @@ const jwtAuthMiddleware = (req, res, next)=>{
 // Function to generate JWT token
 const generateToken = (userData) => {
     // Generate a new JWT token using user data 
-    return jwt.sign(userData, process.env.JWT_SECRET, {expiresIn: 30000});
+    return jwt.sign({userData}, process.env.JWT_SECRET, {espiresIn: 30});
 }
 
 module.exports = (jwtAuthMiddleware, generateToken);
