e18cd249b16a14fe0a21769c4d07045b43fb8a7f
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-09 22:54:11 +0530

    imported user route file in the server.js and used /user route patch for all user routes

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ import bodyParser from 'body-parser';
 app.use(bodyParser.json()); //req.body
 const PORT = process.env.PORT || 3000;
 
+// importing route files
+import userRoutes from './routes/userRoutes.js';
 
+// using the routes
+// app.use is used to set the same route path for all urls
+app.use('/user', userRoutes);
 
 app.listen(PORT, () => {
     console.log('Listening on port 3000...');
