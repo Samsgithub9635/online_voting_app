05916a11941431648c24bc593606b5166e03e6ad
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-10 19:24:01 +0530

    updated models/user.js

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    aadhaarNum: {
+    aadharCardNumber: {
         type: Number,
         required: true,
         unique: true        
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
     isVoted: {
         type: Boolean,
         default: false
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
     }
 });
 
@@ -48,27 +53,27 @@ userSchema.pre('save', async function(next) { //.pre is a middleware function
     const user = this; // everytime we try to save new record it will be at first stored inside user variable
 
     // Hash the password only if it has been modified (or is new)
-   if(!user.isModified('password')) return next(); //if not password modified got to next() therefore no hasing required
-   
+    if(!user.isModified('password')) return next(); //if not password modified got to next() therefore no hasing required
+
     try{
         // hash password generation
 
         // generating salt
         const salt = await bcrypt.genSalt(10); // generating salt using bcryprt library for password hashing, 10 is rounds, rounds represent how complex password will be more the rounds value increases more computational cost increases // 10 is ideal generally
-        
+
         // hash password generation
         const hashPassword = await bcrypt.hash(user.password, salt);
 
         // Override the previous password with the new hashed password
         user.password = hashPassword; 
         next(); //next callback function says to the userSchema(mongoose) every operation tobe done before saving is done now you can save to database   
-    
+
     }catch(err){
         return next(err);
     }
-}) 
+}); 
 
-userSchema.method.comparePassword = async function(candidatePassword){
+userSchema.methods.comparePassword = async function(candidatePassword){
     try{
         // Use bcrypt to compare the provided password with the hashed password
         const isMatch = await bcrypt.compare(candidatePassword, this.password);
@@ -77,7 +82,7 @@ userSchema.method.comparePassword = async function(candidatePassword){
     }catch(err){
         throw err;
     }
-}
+};
 
 const User = mongoose.model('User', userSchema);
-module.exports = User;
+export default User;
