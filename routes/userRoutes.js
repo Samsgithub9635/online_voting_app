4b77e93108259937c38563d15af42420acf4258c
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-10 19:38:21 +0530

    added L44 to send token as response with successful login routes/userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,27 +1,24 @@
 import express, { response } from 'express';
 const router = express.Router();
 import User from '../models/user.js'; 
-import {jwtAuthMiddleware, generateToken} from '../jwt.js' 
+import {jwtAuthMiddleware, generateToken} from '../jwt.js'; 
 
 
 // POST new user that is to signup a new user
 router.post('/signup', async (req, res) => {
     try {
-
         // Create a new user object(newUser) of User type document using Mongoose model
         const newUser = new User(req.body); //req.body: contains the data entered by the users
 
         // save the new user to the database and can be accessed by response
         const response = await newUser.save(); 
-        
-        const payload ={ // payload is created to store user id, username as user data 
+
+        const payload = {
             id: response.id
-        }
+        };
         console.log(JSON.stringify(payload)); // logs the payload which contains user data: id, username in console just for testing
-        
-        // add the payload inside the generated token 
-        // the generated token in jwt.js is stored inside a const variable 'token'
-        const token =generateToken(payload);
+
+        const token = generateToken(payload);
 
         res.status(200).json({response: response, token: token});
     } catch (err) {
@@ -32,22 +29,19 @@ router.post('/signup', async (req, res) => {
 // Login Route
 router.post('/login', async(req, res)=>{
     try{
-        // Extract username and password from request body
         const {aadharCardNumber, password} = req.body;
-
-        // Find the user by username
         const user = await User.findOne({aadharCardNumber: aadharCardNumber});
 
-        // If user does not exist or password does not match, return error
-        if( !user || !(await user.comparePassword(password))){
+        if(!user || !(await user.comparePassword(password))){
             return res.status(401).json({error: 'Invalid aadharCardNumber or password!'});  
-        } 
+        }
 
-        // genrate Token 
         const payload ={
             id: user.id
-        }
+        };
         const token = generateToken(payload);
+
+        res.status(200).json({ token: token });
 
     }catch(err){
         console.log(err);
@@ -56,7 +50,7 @@ router.post('/login', async(req, res)=>{
 });
 
 // Profile Route this is a protected route (jwtAuthMiddleware) used
- router.get('/profile', jwtAuthMiddleware, async (req, res) =>{
+router.get('/profile', jwtAuthMiddleware, async (req, res) =>{
     try{
         const userData = req.user; // comes from line num 14 in jwt.js //extracts user data from payload
         const userId = userData.id; // this id is extracted from the payload
@@ -68,25 +62,20 @@ router.post('/login', async(req, res)=>{
         console.log(err);
         res.status(500).json({error: 'Internal Server Error!'});
     }
- });
-
+});
 
 // Profile password changing route
 router.put('/profile/password', jwtAuthMiddleware, async (req, res) =>{
     try{
+        const userId = req.user.id;
+        const {currentPassword, newPassword} = req.body;
 
-        const userId = req.user.id; // Extract the id from the token
-        const {currentPassword, newPassword} = req.body; // fetch the current password and new password from the client side/postman reqest body 
-
-        //find the user by userId
         const user = await User.findById(userId);
 
-        // if password does not match, return error
         if(!(await user.comparePassword(currentPassword))){
             return res.status(401).json({error: 'Incorrect current password!!!'});
         }
 
-        // Update the user's password
         user.password = newPassword;
         await user.save();
 
@@ -98,9 +87,5 @@ router.put('/profile/password', jwtAuthMiddleware, async (req, res) =>{
         res.status(500).json({error: 'Internal Server Error!'});
     }
 });
-
-
-
-
 
 export default router;
