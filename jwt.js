444787c76953b90fc14899e1684f0a0e60320b3c
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-10 19:29:23 +0530

    minor updates in jwt.js

--- jwt.js
+++ jwt.js
@@ -1,32 +1,30 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const jwtAuthMiddleware = (req, res, next)=>{
-    // Extract the jwt token from the req header
-    // jwt auth middleware
-    const token = req.headers.autorization.split(' ')[1];// splited Bearer token use ' '(space) so that the 'bearer' keyword will be at 0th index and the actual token will be at 1st index
-    if(!token) return res.staus(401).json({error: 'Unauthorized' });
+    try {
+        // Extract the jwt token from the req header
+        // jwt auth middleware
+        const token = req.headers.authorization?.split(' ')[1]; // splited Bearer token use ' '(space) so that the 'bearer' keyword will be at 0th index and the actual token will be at 1st index
+        if(!token) return res.status(401).json({error: 'Unauthorized' });
 
-    try{
         // Verify the JWT token
         const decoded = jwt.verify(token, process.env.JWT_SECRET); //jwt.verify() used to verify the token taking the token and the secret key(JWT_SECRET) and returns the payload
 
         // Attach user information to the request object
-        req.user = decoded; //the decoded payload, want to send it to server
+        req.user = decoded.userData; //the decoded payload, want to send it to server
         next();
-    }catch(err){
+    } catch(err) {
         console.error(err);
-        res.staus(401).json({error: 'Invalid token!'});
+        res.status(401).json({error: 'Invalid token!'});
     }
-
-}
-
+};
 
 // Function to generate JWT token
 const generateToken = (userData) => {
     // Generate a new JWT token using user data 
-    return jwt.sign({userData}, process.env.JWT_SECRET, {espiresIn: 30});
-}
+    return jwt.sign({userData}, process.env.JWT_SECRET, {expiresIn: '30m'});
+};
 
-module.exports = (jwtAuthMiddleware, generateToken);
-
-export default jwtAuthMiddleware;
+export { jwtAuthMiddleware, generateToken };
